628c12c9d5088618ba4b70506d87e0f6dc338e34
Andrew Gene Brown <brown.andrewg@gmail.com>
2023-05-16 06:24:04 -0700

    Update test_crds.R  - add tests for SpatRaster/na.rm

--- inst/tinytest/test_crds.R
+++ inst/tinytest/test_crds.R
@@ -1,7 +1,14 @@
-
+library(terra)
+library(tinytest)
 m <- matrix(c(0,1,0,0,0,1), ncol=2)
 v <- vect(m, type="polygons")
 x <- as.vector(crds(v))
 
 expect_equal(x, c(0,1,0,0,0,0,1,0))
+
+m <- matrix(c(0,1,0,NaN,0,1), ncol=2)
+r <- rast(m)
+expect_equal(nrow(crds(r)), 5)
+expect_equal(nrow(crds(r, na.rm=FALSE)), 6)
+expect_equal(nrow(crds(rast(r))), 6)
 
